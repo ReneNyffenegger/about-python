0fe97c4fde94f9b0638bbcd2fe01a28a2ecca86f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-23 08:30:33 +0200

    + loops_and_lambdas.py

---
+++ gotchas-and-landmines/loops_and_lambdas.py
@@ -0,0 +1,14 @@
+funcs = []
+
+for x in range(5):
+    funcs.append(lambda: x)
+
+print [f() for f in funcs]     #  [4, 4, 4, 4, 4]
+
+# ---
+
+funcs = []
+for x in range(5):
+  funcs.append(lambda x=x: x)
+  
+print [f() for f in funcs]    #  [0, 1, 2, 3, 4]
