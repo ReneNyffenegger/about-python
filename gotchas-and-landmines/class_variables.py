ef195caa208beb4508a65526a4ff875694559fba
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-23 08:24:47 +0200

    + class_variables.py

---
+++ gotchas-and-landmines/class_variables.py
@@ -0,0 +1,31 @@
+#
+#
+#
+
+class AAA(object):
+      x = 1
+
+class BBB(AAA):
+      pass
+
+class CCC(BBB):
+      pass
+
+
+print AAA.x, BBB.x, CCC.x   # 1 1 1
+
+BBB.x = 2
+
+print AAA.x, BBB.x, CCC.x   # 1 2 2
+
+AAA.x = 3
+
+print AAA.x, BBB.x, CCC.x   # 3 2 2
+
+a = AAA()
+
+print a.x, AAA.x            # 3 3
+
+a.x = 4
+
+print a.x, AAA.x            # 4 3
