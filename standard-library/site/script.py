4ec52ed8d4b796e8602a4c9597c6eb99bdb84287
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-24 10:03:39 +0200

    sys.prefix

--- standard-library/site/script.py
+++ standard-library/site/script.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
-import site
 import distutils.sysconfig
+import site
+import sys
 
 print "\nsite.getsitepackages():" 
 for dir in site.getsitepackages():
     print "  " + dir
 
-print "\nCompare with distutils.sysconfig.get_python_lib():"
-print "  " + distutils.sysconfig.get_python_lib()
+print "\n  Compare with distutils.sysconfig.get_python_lib():"
+print "    " + distutils.sysconfig.get_python_lib()
+
+print "\n  Compare alse with sys.prefix"
+print "    " + sys.prefix
