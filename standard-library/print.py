9e64a0ccca46eee1deb4e5de0f9e709202635d78
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-25 10:41:30 +0200

    + print.py

---
+++ standard-library/print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+ar=[1, 2, 3]
+tu=(7, 8, 9)
+
+print ar                 # [1, 2, 3]
+print tu                 # (7, 8, 9)
+
+print "%d %d %d" % tu    # 7 8 9
+
+# print "%d %d %d" % ar  # TypeError: %d format: a number is required, not list
