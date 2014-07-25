9b817f6c16f1bab3a1d4b7639690184b73d78867
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-25 11:54:54 +0200

    format strings

--- types/string/format.py
+++ types/string/format.py
@@ -3,3 +3,6 @@
 print "{} {} {}".format(1, -17, 33)                #  1 -17 33
 print "{:4d} {:4d} {:4d}".format(1, -17, 33)       #     1  -17   33
 print "{:04d} {:04d} {:04d}".format(1, -17, 33)    #  0001 -017 0033
+
+print "{:10s} {:10s}".format('foo', 'bar')         #  foo        bar
+print "{:>10s} {:>10s}".format('foo', 'bar')       #         foo        bar
