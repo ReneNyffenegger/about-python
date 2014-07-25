c8c812ea1a8b360e146099a611f9b23cea4dcc45
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-25 09:55:41 +0200

    Counter.most_common()

--- standard-library/collections/script.py
+++ standard-library/collections/script.py
@@ -7,5 +7,10 @@ cnt=collections.Counter()
 for word in "foo bar baz more or less foo most foo even more foo or is it less".split():
     cnt[word]+=1
 
+print "for word in cnt"
 for word in cnt:
-    print "%-10s: %d" % (word, cnt[word])
+    print "  %-10s: %d" % (word, cnt[word])
+
+print "\nfor word in cnt.most_common(3)"
+for word in cnt.most_common(3):
+    print "  %-10s: %d" % word
