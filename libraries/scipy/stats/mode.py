fadc55abdb6160627caa5cd717368b5f577758db
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 21:06:24 +0200

    4/6

--- libraries/scipy/stats/mode.py
+++ libraries/scipy/stats/mode.py
@@ -1,4 +1,9 @@
+#  Returns an array of the modal (most common) value in the passed array.
+
 import scipy.stats
 
-print scipy.stats.mode( [  4, 5, 4, 6, 4, 2, 4 ])
-# (array([ 4.]), array([ 4.]))
+print scipy.stats.mode( [  4, 5, 4, 6, 4, 2, 4, 4, 4 ])  # 4 is most common, it appears six times
+# (array([ 4.]), array([ 6.]))
+
+
+
