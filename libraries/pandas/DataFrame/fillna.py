ae80182b78b27562e05c60623d21b585f4fe821f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-26 20:58:50 +0200

    inplace=True

--- libraries/pandas/DataFrame/fillna.py
+++ libraries/pandas/DataFrame/fillna.py
@@ -1,8 +1,8 @@
 import pandas
 
 df = pandas.DataFrame(
-      { 'col_1': pandas.Series( [  10 , None,  30  , None ] ),
-        'col_2': pandas.Series( [  'X',  'Y',  'Z' ,  'Q' ] )
+      { 'col_1': pandas.Series( [   10, None,   30, None ] ),
+        'col_2': pandas.Series( [ None,  'Y', None,  'Q' ] )
       })
 
 
@@ -10,7 +10,16 @@ df.col_1 = df.col_1.fillna( 999 )
 
 print df
 #    col_1 col_2
-# 0     10     X
+# 0     10  None
 # 1    999     Y
-# 2     30     Z
+# 2     30  None
+# 3    999     Q
+
+df.col_2.fillna ( '?', inplace=True )
+
+print df
+#    col_1 col_2
+# 0     10     ?
+# 1    999     Y
+# 2     30     ?
 # 3    999     Q
