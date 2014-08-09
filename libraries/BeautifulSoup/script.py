fb9947246bd979623e84a450b4596dabbef8ba3e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-09 11:20:38 +0200

    + BeautifulSoup

---
+++ libraries/BeautifulSoup/script.py
@@ -0,0 +1,18 @@
+from   bs4 import BeautifulSoup
+import requests
+
+html_text=Requests.get('https://github.com/ReneNyffenegger/about-python/tree/master/libraries/BeautifulSoup/script.py').text()
+
+soup = BeautifulSoup(html_text)
+
+print "Title:          ", soup.title
+print "  .name:        ", soup.title.name
+print "  .string       ", soup.title.string
+print "  .parent.name: ", soup.title.parent.name
+
+print
+
+print "Links:"
+
+for a in soup.find_all('a'):
+    print "  %-30s: %s" % (a.string, a.get('href'))
