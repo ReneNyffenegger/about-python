b3556e41f0c5e939bcc67a1ceb3741ce49e2f015
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-09-01 06:40:33 +0200

    Birthday

--- standard-library/datetime/date/script.py
+++ standard-library/datetime/date/script.py
@@ -1,5 +1,12 @@
 import datetime;
 
+
+birthday_ = datetime.date(2014, 8, 28)
+print ("Birthday: " + birthday_.strftime("%Y-%m-%d"))
+
+
+# 
+
 today_     = datetime.date.today()
 in_a_week_ = today_ + datetime.timedelta(days = 7)
 
