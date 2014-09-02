e58a2da0b184d9786342eee894347a9edb9f3adc
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-09-02 10:15:02 +0200

    urllib.urlretrieve

--- standard-library/urllib/script.py
+++ standard-library/urllib/script.py
@@ -5,5 +5,9 @@
 #
 import urllib
 
-html_text = urllib.urlopen('https://github.com/ReneNyffenegger/about-python/tree/master/standard-library/urllib').read()
+html_text = urllib.urlopen('https://raw.githubusercontent.com/ReneNyffenegger/about-python/master/standard-library/urllib/script.py').read()
 print html_text
+
+# --
+
+urllib.urlretrieve ('https://raw.githubusercontent.com/ReneNyffenegger/about-python/master/standard-library/urllib/script.py', 'downloaded-script.py');
