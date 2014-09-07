0450e670356fced4981007948da67b3cc27946cf
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-09-07 21:32:04 +0200

    + transactions.py

---
+++ standard-library/sqlite3/transactions.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+
+if os.path.isfile('trx.db'):
+   os.remove('trx.db')
+
+db = sqlite3.connect('trx.db')
+
+cur = db.cursor()
+
+cur.execute('create table t (a number, b varchar)')
+
+cur.execute("insert into t values (1, 'one')")
+cur.execute("insert into t values (2, 'two')")
+
+db.rollback()
+
+cur.execute("insert into t values (3, 'three')")
+cur.execute("insert into t values (4, 'four')")
+
+db.commit()
+
+
+for row in cur.execute('select * from t order by a'):  
+    print "%2d: %s" % (row[0], row[1])
