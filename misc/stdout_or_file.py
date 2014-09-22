ca0579b9a8bc507183c978827f1f8cbb1968439f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-09-22 07:06:12 +0200

    + comment

--- misc/stdout_or_file.py
+++ misc/stdout_or_file.py
@@ -1,3 +1,20 @@
+#
+#      To be called either so
+#
+#         python stdout_or_file.py
+#
+#      or so
+#
+#         python stdout_or_file.py some-file-name
+#
+#      -------------------------------------------
+#
+#      If no file name is specified, the script will
+#      print three lines to stdout, otherwise, it
+#      will print these lines into the file given
+#      as first argument to the script.
+#
+#
 import sys
 
 if len(sys.argv) > 1:
