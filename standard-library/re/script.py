a47e365f26a85349db6e5c61074e4aa2f1f89faf
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-10-06 10:28:33 +0200

    more re.search

--- standard-library/re/script.py
+++ standard-library/re/script.py
@@ -47,6 +47,14 @@ else:
 
 
 print "---"
+match = re.search('foo:\s+(\d+),\s+bar:\s+(\d+)', 'hello foo:   42, bar:  1001xyz')
+if match:
+   print match.group()   # foo:   42, bar:  1001
+   print match.group(1)  # 42
+   print match.group(2)  # 1001
+
+
+print "---"
 
 for found in re.findall(r'(\w+)\s+(\d+)', 'foo 42 bar 18 baz 19 x'):
     print found[0] + ': ' + found[1]
