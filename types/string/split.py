2041953a86a8b63ebbf833719d3f4a61ddfe5ea9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-10-31 23:01:45 +0100

    split(',')

--- types/string/split.py
+++ types/string/split.py
@@ -2,5 +2,25 @@
 
 str="hello world, the famous number is forty-two, is it not?"
 
+print 'str.split()'
 for word in str.split():
     print '  ' + word
+
+#  hello
+#  world,
+#  the
+#  famous
+#  number
+#  is
+#  forty-two,
+#  is
+#  it
+#  not?
+
+print "str.split(',')"
+for part in str.split(','):
+    print '  ' + part
+
+#  hello world
+#   the famous number is forty-two
+#   is it not?
