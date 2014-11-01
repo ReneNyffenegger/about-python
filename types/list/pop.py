e0734413bb6751b6ada52c32b2d05e802b8f5a49
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-11-01 01:13:31 +0100

    + list.pop

---
+++ types/list/pop.py
@@ -0,0 +1,16 @@
+L = ['one', 'two', 'three', 'four', 'five']
+
+p = L.pop()
+
+print (p)
+# five
+
+print (L)
+# ['one', 'two', 'three', 'four']
+
+p = L.pop(2)
+print (p)
+# three
+
+print (L)
+# ['one', 'two', 'four']
