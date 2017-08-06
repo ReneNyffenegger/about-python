d16098339ce55a4042832c4510cd24e6c1ec4340
René Nyffenegger <mail@renenyffenegger.ch>
2017-08-06 14:30:35 +0200

    not a deep copy

--- misc/copy_array.py
+++ misc/copy_array.py
@@ -1,5 +1,10 @@
 a      =  [ 'one', 'two', 'three', 'four' ]
+
+# Make a copy of a. Note: not a deep copy because references within
+# point to the same object.
 a_copy = a[:]
+
+# Create a reference to the array pointed at by 'a'.
 a_ref  = a
 
 a_copy.append('five')
