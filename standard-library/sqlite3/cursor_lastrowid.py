fb6c912313c9ee114de873cf983462a722965682
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-03-31 15:42:16 +0200

    + cursor_lastrowid.py

---
+++ standard-library/sqlite3/cursor_lastrowid.py
@@ -0,0 +1,22 @@
+#
+#    cursor.lastrowid returns tha last inserted rowid
+#
+import os
+import sys
+import sqlite3
+
+db_filename = 'cursor_lastrowid.db'
+if os.path.isfile(db_filename):
+   os.remove(db_filename)
+
+def ins(a):
+    cur.execute('insert into t(a) values (?)', (a, ))
+    print('Inserted a record, lastrowid is ' + str(cur.lastrowid))
+
+db = sqlite3.connect(db_filename)
+cur = db.cursor()
+
+cur.execute('create table t (id integer primary key, a)')
+
+ins(42)
+ins(99)
