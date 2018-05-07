b5e27bb2d4c8489b0c8c5dd9f65fc052cee659ea
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-05-07 10:40:54 +0200

    + version_info.py

---
+++ standard-library/sys/version_info.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+
+print(sys.version_info.major)
+print(sys.version_info.minor)
+print(sys.version_info.micro)
+print(sys.version_info.releaselevel)
+print(sys.version_info.serial)
