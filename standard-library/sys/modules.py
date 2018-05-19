edc28a6d0f9e0f2e8e9153ddf2e170c9079666f6
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-05-19 15:16:38 +0200

    show modules

--- standard-library/sys/modules.py
+++ standard-library/sys/modules.py
@@ -3,12 +3,17 @@
 import sys
 
 #
-# The type of sys.modules is a dictionary:
+#   The type of sys.modules is a dictionary:
 #
 print(type(sys.modules)) # <class 'dict'>
 
 #
+#   Print names of imported modules.
+#
+for module_name in sys.modules:
+    print(module_name)
+
+#
 # sys.modules maps from strings to 'class modules':
 #
-for mod in sys.modules:
-    print(type(sys.modules[mod]))
+print(type(sys.modules['sys'])) # <class 'module'>
