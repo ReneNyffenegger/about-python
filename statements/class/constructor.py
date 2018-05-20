55845dafffe011d447d74f8c3b67c95750781c0e
René Nyffenegger <mail@renenyffenegger.ch>
2018-05-20 20:58:28 +0200

    Some comments

--- statements/class/constructor.py
+++ statements/class/constructor.py
@@ -3,6 +3,12 @@
 class ABC:
 
     def __init__(self, x, y):
+    #
+    #   The class's constructor.
+    #   The first parameter is automatically aliased
+    #   to the instance being constructed.
+    #
+        print('Constructor being executed')
         self.x = x
         self.y = y
 
@@ -10,5 +16,11 @@ class ABC:
         print('x = {:d}, y = {:s}'.format(self.x, self.y))
 
 
+#
+#   Initialize an instance of a class.
+#   Note: there is no 'new' statement, using
+#   the class name with following paranthesis
+#   is sufficient.
+#
 abc = ABC(42, 'foo')
 abc.show()
