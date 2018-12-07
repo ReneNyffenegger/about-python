b1a7d7f5462466ab8c9bd96d6e9201fee7a6c17b
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-07 22:46:07 +0100

    fix print / wrong number

--- gotchas-and-landmines/default_arguments.py
+++ gotchas-and-landmines/default_arguments.py
@@ -1,23 +1,24 @@
+#!/usr/bin/python3
 #
 #      http://stackoverflow.com/a/530768/180275
 #
 import time
 
 def report_time_wrong(when=time.time()):
-    print when
+    print (when)
 
-report_time_wrong()   # 1406096376.71
+report_time_wrong()   # 1544219102.503972
 time.sleep(5)
                       # Note, same time again
-report_time_wrong()   # 1406096376.71
+report_time_wrong()   # 1544219102.503972
 
 
 def report_time_better(when=None):
     if when is None:
        when = time.time()
 
-    print when
+    print (when)
 
-report_time_better()  # 1406096381.71
+report_time_better()  # 1544219107.5091524
 time.sleep(5)
-report_time_better()  # 1406096381.71
+report_time_better()  # 1544219112.5136003
