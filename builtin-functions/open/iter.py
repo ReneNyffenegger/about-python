88649aa8240e0ebbc1101f83f95c3dae5cdb250b
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-08 10:15:36 +0100

    + iter.py

---
+++ builtin-functions/open/iter.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+with open('iter.txt') as txt:
+    for line in iter(txt.readline, ''):
+      # Remove trailing new line because…
+        line = line.rstrip('\n')
+      # … it is implicitely added in print()
+        print(line)
