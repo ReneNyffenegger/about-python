26e63265d1b563133ae8cfb0695649f59e2b1ebf
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-13 17:19:13 +0100

    python3

--- types/list/list_comprehension.py
+++ types/list/list_comprehension.py
@@ -1,10 +1,11 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
 
 squares = [i**2 for i in range(11)] # compare with « map(lambda i: i**2, range(11)) »
-print squares
+print(squares)
 # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 
 squares_of_even_numbers = [i**2 for i in range(11) if i % 2 ==0]
-print squares_of_even_numbers
+print(squares_of_even_numbers)
 # [0, 4, 16, 36, 64, 100]
