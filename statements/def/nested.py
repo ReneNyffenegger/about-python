b10ab9cb62e86927a737bc3ed7e3f94b1977cc38
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-14 15:51:55 +0100

    doubly_nested_func

--- statements/def/nested.py
+++ statements/def/nested.py
@@ -7,9 +7,15 @@ def func():
     def nested_func():
         print("nested_func was called")
 
+        def doubly_nested_func():
+            print("doubly_nested_func was called")
+
+        doubly_nested_func()
+
     nested_func()
 
 func()
 #
 # func was called
 # nested_func was called
+# doubly_nested_func was called
