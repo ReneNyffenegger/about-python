4de105e88dbaf4b8fbf1b530db5cb6710f669fee
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-14 16:55:36 +0100

    more comments

--- statements/class/statements.py
+++ statements/class/statements.py
@@ -8,13 +8,29 @@ print("next is definition of class TQ84")
 
 class TQ84():
 
+ # 
+ #  The following two statements (print and calling globalFunc)
+ #  are executed while this class definition is read by the
+ #  parser (and not, as one might think) when the class
+ #  is instantiaed.
+ # 
     print("Within TQ84")
     x = globalFunc()
 
     def __init__(self, name):
+     #
+     #  However, these two statements are
+     #  only executed when an instance of
+     #  the class is created (but the def
+     #  is executed)
+     #
         print("TQ84.__init__ was called")
         self.name = name
 
+ #
+ #  Again, this print statement is executed
+ #  during parsing of the class.
+ #
     print("This line is after def __init__")
 
 print("going to instantiate an instance")
