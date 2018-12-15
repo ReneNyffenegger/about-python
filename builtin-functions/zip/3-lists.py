ff5edc32d81ea3df090e1a1ed8eb87f163cc40a8
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-15 15:09:12 +0100

    + 3-lists.py

---
+++ builtin-functions/zip/3-lists.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+numbers = [ 1   ,  2   ,  3     ,  4    ,  5    ]
+letters = ['a'  , 'b'  , 'c'    , 'd'   , 'e'   ]
+words   = ['one', 'two', 'three', 'four', 'five']
+
+zipped = zip(numbers, letters, words)
+for triple in zipped:
+    print(triple)
+#
+# (1, 'a', 'one')
+# (2, 'b', 'two')
+# (3, 'c', 'three')
+# (4, 'd', 'four')
+# (5, 'e', 'five')
