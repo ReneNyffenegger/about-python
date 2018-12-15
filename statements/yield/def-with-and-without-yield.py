4aa5a5ae957f1fc66c3160d00b70aaab919625d5
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-15 10:26:36 +0100

    write output after statement

--- statements/yield/def-with-and-without-yield.py
+++ statements/yield/def-with-and-without-yield.py
@@ -8,16 +8,16 @@ def FuncWithYield():
     yield 42
 
 print(type(FuncWithoutYield))
-print(type(FuncWithYield))
-#
 # <class 'function'>
+
+print(type(FuncWithYield))
 # <class 'function'>
 
 retFuncWithoutYield = FuncWithoutYield()
 retFuncWithYield    = FuncWithYield()
 
 print(type(retFuncWithoutYield))
-print(type(retFuncWithYield))
-#
 # <class 'int'>
+
+print(type(retFuncWithYield))
 # <class 'generator'>
