ed9f980429ca9b16c701f7ed84cc87397808a56a
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-26 08:23:30 +0100

    script -> sleep

--- standard-library/time/script.py
+++
@@ -1,6 +0,0 @@
-import time
-
-t_start = time.time()
-print "Time going to sleep for four seconds."
-time.sleep(4)
-print "Just woke up, after {:f} seconds".format(time.time() - t_start)
---
+++ standard-library/time/sleep.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import time
+
+t_start = time.time()
+print("Time going to sleep for four seconds.")
+time.sleep(4)
+print("Just woke up, after {:f} seconds".format(time.time() - t_start))
