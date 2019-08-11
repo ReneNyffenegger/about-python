4e8a076cdff970750b0b2886d58ab6221b2ea032
René Nyffenegger <mail@renenyffenegger.ch>
2019-08-11 21:23:39 +0200

    More comments

--- misc/read-csv/go.py
+++ misc/read-csv/go.py
@@ -13,12 +13,18 @@ for line in csv:
 
     line = line.rstrip()
 
+  #
+  # Remove the trailing new line
+  #
     values=line.split(',')
 
     val_1 = values[0]
     val_2 = values[1]
     val_3 = values[2]
 
+  #
+  # Make empty values visually stand out
+  #
     if val_1 == '':
        val_1 =  '-'
     if val_2 == '':
@@ -26,5 +32,7 @@ for line in csv:
     if val_3 == '':
        val_3 =  '-'
 
+  #
+  # Print the values read from the CSV file
+  #
     print(' {:<5} | {:<5} | {:<5}'.format(val_1, val_2, val_3))
-
