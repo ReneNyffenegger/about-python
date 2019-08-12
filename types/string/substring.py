aaa516e1970c5bf9c057169efd6c675d8d1195db
René Nyffenegger <mail@renenyffenegger.ch>
2019-08-12 20:59:55 +0200

    + substring.py

---
+++ types/string/substring.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+txt = '0123456789'
+
+print(txt[ 3  ]) # 4th character                - 3
+print(txt[ 3:7]) # From 4th to 7th character    - 3456
+print(txt[ 7: ]) # From 8th character to end    - 789
+print(txt[-2  ]) # 2nd last character           - 8
+print(txt[-4: ]) # 4th last character to end    - 6789
