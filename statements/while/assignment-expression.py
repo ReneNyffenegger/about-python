daee24baf40f1ca8ce3f73f4e4f70da6c6814731
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-26 10:36:41 +0100

    + assignment-expression

---
+++ statements/while/assignment-expression.py
@@ -0,0 +1,11 @@
+import random
+
+random.seed('tq84')
+
+while (rnd := random.random()) < 0.9:
+      print('rnd = {0}'.format(rnd))
+#
+#     rnd = 0.15919424924070613
+#     rnd = 0.4948802376988537
+#     rnd = 0.6212905728914367
+#     rnd = 0.5917838981303569
