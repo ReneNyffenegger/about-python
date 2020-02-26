686a13e635024565314c7c899e259cfac40519d4
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-26 10:13:11 +0100

    + break-continue.py

---
+++ statements/while/break-continue.py
@@ -0,0 +1,15 @@
+x = 0
+
+while True:
+
+      x += 1
+
+      if x == 2:
+         print('x = 2 -> continue')
+         continue
+
+      if x == 5:
+         print('x = 5 -> break')
+         break
+
+      print('end of loop, x = {0}'.format(x))
