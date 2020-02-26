603113e805849f12e984b6ba34fbcddf03d1453a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-26 21:15:07 +0100

    + Empty/import-Empty.py

---
+++ statements/import/import-Empty.py
@@ -0,0 +1,23 @@
+import Empty
+
+print(dir(Empty))
+#
+#  ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
+
+
+print(Empty.__name__)
+#
+#  Empty
+
+print(Empty.__package__)
+#
+#
+
+print(Empty.__file__)
+#
+#  C:\Users\rene\about\Python\statements\import\Empty.py
+
+
+print(Empty.__doc__)
+#
+#  None
