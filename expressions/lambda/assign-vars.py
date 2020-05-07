c4ef749acfad5aaf702652ad38090607d154b12e
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-07 13:15:15 +0200

    variables in lambdas

---
+++ expressions/lambda/assign-vars.py
@@ -0,0 +1,9 @@
+val = 2
+
+times_three = lambda v=val: v *  3
+squared     = lambda v=val: v ** 2
+
+val = 4
+
+print(times_three())
+print(squared())
---
+++ expressions/lambda/global-vars.py
@@ -0,0 +1,9 @@
+val = 2
+
+times_three = lambda: val *  3
+squared     = lambda: val ** 2
+
+val = 4
+
+print(times_three())
+print(squared())
