b7b079b2efee68739d9db086bc56a0928e9e82cd
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-07 11:39:48 +0200

    remove comment

--- statements/global/2-assign-to-var.py
+++ statements/global/2-assign-to-var.py
@@ -1,13 +1,8 @@
 num = 42
 
 def printNumAndIncrement(i):
-  #
-  # The following print statement throws
-  #
-  #   UnboundLocalError: local variable 'num' referenced before assignment
-  #
     print(num)
-  
     num = num + i
 
 printNumAndIncrement(57)
+printNumAndIncrement( 1)
