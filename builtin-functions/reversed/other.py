5b76fbd40747877c44abb9fb1836ff0d08cac8d2
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-08 20:57:52 +0200

    + other.py

---
+++ builtin-functions/reversed/other.py
@@ -0,0 +1,16 @@
+for char in reversed('olleH'):
+    print(char)
+#
+#  H
+#  e
+#  l
+#  l
+#  o
+
+for word in reversed(['world', 'hello', 'say', 'I']):
+    print(word)
+#
+#  I
+#  say
+#  hello
+#  world
