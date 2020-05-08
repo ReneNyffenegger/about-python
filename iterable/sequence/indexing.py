6d6f4025a8b4bb01fe14d2b20e1fa816b6be86bf
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-08 21:39:49 +0200

    + indexing.py

---
+++ iterable/sequence/indexing.py
@@ -0,0 +1,29 @@
+numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six']
+
+print(numbers[4])
+#
+# four
+
+print(numbers[2:5])
+#
+# ['two', 'three', 'four']
+
+print(numbers[-2])
+#
+# five
+
+print(numbers[-3:-1])
+#
+# ['four', 'five']
+
+print(numbers[-3:])
+#
+# ['four', 'five', 'six']
+
+print(numbers[5:])
+#
+# ['five', 'six']
+
+print(numbers[:])
+#
+# ['zero', 'one', 'two', 'three', 'four', 'five', 'six']
