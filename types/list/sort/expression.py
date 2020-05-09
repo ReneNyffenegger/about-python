608a43aa0496b5132bd051a952a05d927a70a2b4
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-09 21:53:29 +0200

    + expression.py

---
+++ types/list/sort/expression.py
@@ -0,0 +1,7 @@
+words = [ 'appple', 'car', 'quit', 'oh', 'thunderstorm', 'academy' ]
+
+words.sort(key = lambda w: len(w))
+
+print(words)
+#
+#  ['oh', 'car', 'quit', 'appple', 'academy', 'thunderstorm']
