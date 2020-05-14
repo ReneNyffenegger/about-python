4c44bc73318530a1612b1a6ad716c624305a582c
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-14 09:13:26 +0200

    + slicing.py

---
+++ types/list/slicing.py
@@ -0,0 +1,25 @@
+lst = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six' ]
+
+print( lst[4:] )
+#
+# ['four', 'five', 'six']
+
+print( lst[:4] )
+#
+# ['zero', 'one', 'two', 'three']
+
+print( lst[1:6] )
+#
+# ['one', 'two', 'three', 'four', 'five']
+
+print( lst[1:-1] )
+#
+# ['one', 'two', 'three', 'four', 'five']
+
+print( lst[1:-1:2] )
+#
+# ['one', 'three', 'five']
+
+print( lst[:] )
+#
+# ['zero', 'one', 'two', 'three', 'four', 'five', 'six']
