fb743683af67c4f2c5952088debd2891d475b3c1
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-15 21:58:20 +0200

    n/a

--- types/list/comprehension/2D.py
+++ types/list/comprehension/2D.py
@@ -1,6 +1,6 @@
 grid = [
-         [ x * y for x in range(1, 5)]  # <- Create lists with 4 elements
-                 for y in range(0, 3)   # <- Create lists with 3 elements
+         [ x * y for x in range(1, 5)]  # <- Create a list with 4 elements, y is determined by the following list comprehension
+                 for y in range(0, 3)   # <- Create 3 of the previous expression (which is a list of 4 elements)
        ]
 
 print(grid)
