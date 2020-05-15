3d8b70ed02ef8410642cba2edcbd30b990ebfdf3
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-15 20:54:52 +0200

    remove she-bang

--- types/list/comprehension/criterion.py
+++ types/list/comprehension/criterion.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
 import math
 
 print ( [ math.sqrt(x) for x in [4,-16,25,64,-28] if x >= 0 ] )
+#
 # [2.0, 5.0, 8.0]
--- types/list/comprehension/squares.py
+++ types/list/comprehension/squares.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-
 squares = [i**2 for i in [2,4,5,7] ]
 print(squares)
+#
 # [4, 16, 25, 49]
