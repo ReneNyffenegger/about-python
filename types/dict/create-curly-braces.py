1b12753cb093f9471116e9e9935cc59a13df4c67
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-16 23:27:16 +0200

    + create-curly-braces.py

---
+++ types/dict/create-curly-braces.py
@@ -0,0 +1,13 @@
+empty_dict = {}
+another_dict = {
+  'one'              :  1,
+  (42, 'hello world'): 'foo, bar and baz',
+   99                : 'Nena'} 
+
+print(type(empty_dict  ))
+#
+#  <class 'dict'>
+
+print(type(another_dict))
+#
+#  <class 'dict'>
