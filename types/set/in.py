813e73377c90fa03e30898abef992cce0cf46a52
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-16 20:37:47 +0200

    + in.py

---
+++ types/set/in.py
@@ -0,0 +1,11 @@
+prime = {2, 3, 5, 7, 11, 13}
+
+if 4 in prime:
+   print('4 is a prime')
+else:
+   print('4 is not a prime')
+
+if 6 not in prime:
+   print('6 is not a prime')
+else:
+   print('6 is a prime')
--- types/set/set-operators.py
+++ types/set/set-operators.py
@@ -1,19 +1,19 @@
-even = { 2, 4, 6, 8, 10, 12 }
-odd  = { 1, 3, 5, 7,  9, 11 }
-prim = { 2, 3, 5, 7, 11     }
+even  = { 2, 4, 6, 8, 10, 12 }
+odd   = { 1, 3, 5, 7,  9, 11 }
+prime = { 2, 3, 5, 7, 11     }
 
-print(prim - odd)
+print(prime - odd)
 #
 # {2}
 
-print(prim & even)
+print(prime & even)
 #
 # {2}
 
-print(even | prim)
+print(even | prime)
 #
 # {2, 3, 4, 5, 6, 7, 8, 10, 11, 12}
 
-print(odd ^ prim)
+print(odd ^ prime)
 #
 # {1, 2, 9}
