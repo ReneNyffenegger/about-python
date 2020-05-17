12aa0d2ed3079266183921d2229e36628949dd5f
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-17 19:56:21 +0200

    remove shebang, add comments

--- builtin-functions/open/iterate-over-each-line-in-file.py
+++ builtin-functions/open/iterate-over-each-line-in-file.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
-
 with open('iter.txt') as txt_file:
      for txt_line in txt_file:
-         print (txt_line, end='') # use end='' to omit printing new-lines
+      #
+      #  use end='' to omit printing new-lines
+      # (The newline is already present in txt_line)
+      #
+         print (txt_line, end='')
