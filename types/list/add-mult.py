ea1ccf598e84b1211a484dae41815be1aba6db69
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-17 22:42:11 +0200

    + add-mult.py

---
+++ types/list/add-mult.py
@@ -0,0 +1,10 @@
+lst_1 = ['one' , 'two' , 'three']
+lst_2 = ['four', 'five'         ]
+
+print(lst_1 + lst_2)
+#
+#  ['one', 'two', 'three', 'four', 'five']
+
+print(3 * lst_2)
+#
+#  ['four', 'five', 'four', 'five', 'four', 'five']
