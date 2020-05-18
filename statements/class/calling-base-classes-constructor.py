5833b892c3d567c8b4ffff4aab73773202fd0de3
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-18 14:16:32 +0200

    remove shebang, add ident

--- statements/class/calling-base-classes-constructor.py
+++ statements/class/calling-base-classes-constructor.py
@@ -1,27 +1,31 @@
-#!/usr/bin/python
-
-
 class A:
-  def __init__(self):
-      print("  A's constructor")
+  def __init__(self, ident):
+      print(f"  A's constructor, ident = {ident}")
+      self.ident = ident
+
+  def printIdent(self):
+      print(self.ident)
 
 
 class B(A):
 
-  def __init__(self):
+  def __init__(self, ident):
   #   Call A's constructor:
-      A.__init__(self)
       print("  B's constructor")
+      A.__init__(self, ident)
         
 
 class C(A):
 
-  def __init__(self):
+  def __init__(self, ident):
   #   Note: A's constructor is not called
       print("  C's constructor")
 
 print('Creating a B')
-b = B()
+b = B(42)
 
 print('Creating a C')
-c = C()
+c = C(49)
+
+b.printIdent()
+# c.printIdent()
