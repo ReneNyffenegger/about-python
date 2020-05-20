a71737ab6ceabc1cc6a7671e50ec84d09c3a6d89
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-20 10:39:44 +0200

    print -> print()

--- builtin-functions/print/demo.py
+++ builtin-functions/print/demo.py
@@ -1,18 +1,16 @@
-#!/usr/bin/python
-
-print "%f"    % 3.141             # 3.141000
-print "%6.2f" % 3.141             #   3.14
-print "%06.2f" % 3.141            # 003.14  
+print("%f"     % 3.141)           # 3.141000
+print("%6.2f"  % 3.141)           #   3.14
+print("%06.2f" % 3.141)           # 003.14  
 
 ar=[1, 2, 3]
 tu=(7, 8, 9)
 
-print ar                          # [1, 2, 3]
-print tu                          # (7, 8, 9)
+print(ar)                         # [1, 2, 3]
+print(tu)                         # (7, 8, 9)
 
-print "%d %d %d"    %  tu         # 7 8 9
+print("%d %d %d"    %  tu)        # 7 8 9
 
-print "{} {}".format(tu, 5)       # (7, 8, 9) 5
+print("{} {}".format(tu, 5))      # (7, 8, 9) 5
 
 
-# print "%d %d %d" % ar  # TypeError: %d format: a number is required, not list
+# print("%d %d %d" % ar)  # TypeError: %d format: a number is required, not list
