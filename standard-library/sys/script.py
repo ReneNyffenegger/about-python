b4e5dfab5313d4ab42d893e751a5c60790a34afa
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-20 10:37:05 +0200

    print -> print()

--- standard-library/sys/script.py
+++ standard-library/sys/script.py
@@ -1,23 +1,18 @@
-#!/usr/bin/python
-#
-#   Links:
-#     ../../misc/stdout_or_file.py
-
 import sys
 
-print "\npaths in sys.path:"
+print("\npaths in sys.path:")
 for dir in sys.path:
-    print "  " + dir
+    print("  " + dir)
 
 
-print "\nPrint without newlines:"
+print("\nPrint without newlines:")
 sys.stdout.write('  ')
 for i in range(10):
     sys.stdout.write (str(i) + " ")
 
-print "\n\nsys.prefix = " + sys.prefix
-print "  Compare with distutils.sysocnifg.get_python_lib() and site.getsitepackages()"
+print("\n\nsys.prefix = " + sys.prefix)
+print("  Compare with distutils.sysocnifg.get_python_lib() and site.getsitepackages()")
 
-print "\ngetdefaultencoding: " + sys.getdefaultencoding()
+print("\ngetdefaultencoding: " + sys.getdefaultencoding())
 
 sys.exit(0)
