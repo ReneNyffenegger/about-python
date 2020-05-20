46636aec4ac031bfb2a48291e05da93de5eefd81
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-20 10:24:17 +0200

    print(), remove comments

--- standard-library/sys/stdout_or_file.py
+++ standard-library/sys/stdout_or_file.py
@@ -1,24 +1,7 @@
-#
-#      To be called either so
-#
-#         python stdout_or_file.py
-#
-#      or so
-#
-#         python stdout_or_file.py some-file-name
-#
-#      -------------------------------------------
-#
-#      If no file name is specified, the script will
-#      print three lines to stdout, otherwise, it
-#      will print these lines into the file given
-#      as first argument to the script.
-#
-#
 import sys
 
 if len(sys.argv) > 1:
-   print "Writing to file " + sys.argv[1]
+   print("Writing to file " + sys.argv[1])
 
    f_ = open(sys.argv[1], 'w')
 
@@ -29,3 +12,6 @@ else:
 f_.write('Line one\n')
 f_.write('Line two\n')
 f_.write('Line three')
+
+if len(sys.argv) > 1:
+   f_.close()
