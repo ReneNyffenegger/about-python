09160a410cd8e864bf5908197958eb263367a1bc
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-22 20:56:29 +0200

    + for-in.py

---
+++ iterable/unpacking/for-in.py
@@ -0,0 +1,9 @@
+for foo, *bar, baz in [
+      ( 'one', 'two', 'three', 'four', 'five' ),
+      [    1 ,    2 ,      3 ,             5  ],
+    ]:
+
+    print(foo)
+    print(bar)
+    print(baz)
+    print()
