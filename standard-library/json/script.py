62fbf563e05008da84a701aa1476f69bf3a144b2
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-23 09:42:11 +0200

    reformat / remove shebang

--- standard-library/json/script.py
+++ standard-library/json/script.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 import json
 
 #
@@ -6,13 +5,18 @@ import json
 #
 d = json.loads ('''
 
-  {"foo":{"42":"forty-two",
-           "8":"eight"},
-    "bar":[{"key":"1"},
-           {"key":"2"},
-           {"key":"3"},
-           {"key":"4"}]
-   }
+  {
+    "foo":{
+       "42":"forty-two",
+       "8" :"eight"
+    },
+    "bar":[
+       {"key":"1"},
+       {"key":"2"},
+       {"key":"3"},
+       {"key":"4"}
+    ]
+  }
   
 ''')
 
