1b132a1d04bf84a7a9369ef9555e5036393469ef
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-27 22:49:54 +0200

    appendleft()

--- standard-library/collections/deque/demo.py
+++ standard-library/collections/deque/demo.py
@@ -1,10 +1,36 @@
 from collections import deque
 
 D = deque([ 'one', 'two', 'three', 'four' ])
+
+#
+#   Add items on the deque's right side:
+#
 D.append('five')
+
+#
+#   Add another item (which happens to be a list):
+#
 D.append([ 'six', 'seven' ])
 
-print(D.popleft()) # one
-print(D.popleft()) # two
+#
+# Add an on the deque's left side:
+#
+D.appendleft('zero')
 
-print(D) # deque(['three', 'four', 'five', ['six', 'seven']])
+print(D)
+#
+#  deque(['zero', 'one', 'two', 'three', 'four', 'five', ['six', 'seven']])
+
+
+#
+# Remove items on the right side
+#
+print(D.pop())     # ['six', 'seven']
+print(D.pop())     #  five
+
+#
+# Remove an item on the left side
+#
+print(D.popleft()) # zero
+
+print(D)           #  deque(['one', 'two', 'three', 'four'])
