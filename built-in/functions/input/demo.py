80c63b1369900b0cc7b6e5262e73ebafe3bc9b35
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-06-15 17:35:11 +0200

    + demo.py

---
+++ built-in/functions/input/demo.py
@@ -0,0 +1,7 @@
+username = input('Name:     ')
+password = input('Password: ')
+
+if username == 'Rene' and password == 'secretGarden':
+   print('Bingo')
+else:
+   print('Wrong username or password')
