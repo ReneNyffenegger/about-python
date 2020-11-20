4ce4fac314a7c24c1485672dbaed66d4b46e4177
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-11-20 08:29:03 +0100

    Add default value and required value

--- standard-library/argparse/simple.py
+++ standard-library/argparse/simple.py
@@ -2,8 +2,10 @@ import argparse
 
 argParser = argparse.ArgumentParser()
 
-argParser.add_argument('-flag'   , action = 'store_true', help = 'Specify a true/false option')
-argParser.add_argument('-number' , type   =  int        , help = 'What is the number')
+argParser.add_argument('-flag'   , action = 'store_true', help = 'Specify a true/false option'                          )
+argParser.add_argument('-number' , type   =  int        , help = 'What is the number'                                   )
+argParser.add_argument('-text'   , type   =  str        , help = 'What is the text'           , default  = 'Hello world')
+argParser.add_argument('-req'    , type   =  int        , help = 'A required argument'        , required =  True        )
 
 parsedArgs = argParser.parse_args()
 
@@ -13,3 +15,5 @@ else:
    print('You did not specify the flag')
 
 print('The number is ' + str(parsedArgs.number))
+print('The text   is ' +     parsedArgs.text   )
+print('req        is ' + str(parsedArgs.req   ))
