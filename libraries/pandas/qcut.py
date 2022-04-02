62fbe1e26c47f91d0d6ea35389100339f5da11bf
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-04-02 13:00:40 +0200

    print -> print()

--- libraries/pandas/qcut.py
+++ libraries/pandas/qcut.py
@@ -2,21 +2,11 @@ import pandas
 
 q =  pandas.qcut ( [ 1, 2, 2, 4, 5, 9, 10, 12, 15, 16, 16, 18, 20, 23 ], 4)
 
-print type(q)
-# <class 'pandas.core.categorical.Categorical'>
+print(type(q))
+# <class 'pandas.core.arrays.categorical.Categorical'>
 
-print q
-#   [1, 4.25]
-#   [1, 4.25]
-#   [1, 4.25]
-#   [1, 4.25]
-#  (4.25, 11]
-#  (4.25, 11]
-#  (4.25, 11]
-#    (11, 16]
-#    (11, 16]
-#    (11, 16]
-#    (11, 16]
-#    (16, 23]
-#    (16, 23]
-#    (16, 23]
+print(q)
+#
+# Length: 14
+# Categories (4, interval[float64, right]): [(0.999, 4.25] < (4.25, 11.0] < (11.0, 16.0] <
+#                                           (16.0, 23.0]]
