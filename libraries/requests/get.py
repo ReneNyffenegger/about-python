b71fe9bebe35ed41ff351b57b267b1d6537aed80
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-04-02 11:47:54 +0200

    print -> print()

--- libraries/requests/get.py
+++ libraries/requests/get.py
@@ -3,14 +3,14 @@ import requests
 
 gotten = requests.get('https://raw.githubusercontent.com/ReneNyffenegger/about-python/master/libraries/requests/get.py')
 
-print 'Status code: ', gotten.status_code
-print "Headers:";
+print('Status code: ', gotten.status_code)
+print('Headers:');
 
 for header in gotten.headers:
-    print "  %-30s: %s" % (header, gotten.headers[header])
+    print("  %-30s: %s" % (header, gotten.headers[header]))
 
-print 'Encoding: ', gotten.encoding
+print('Encoding: ', gotten.encoding)
 
-print
+print()
 
-print gotten.text
+print(gotten.text)
