9c3ad08e4df37b012e008439f6a41c2a233075d9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-04-02 11:37:34 +0200

    print ->print()

--- libraries/BeautifulSoup/recursively.py
+++ libraries/BeautifulSoup/recursively.py
@@ -9,10 +9,10 @@ def descend(node, level):
     for child in node.contents:
 
         if child.name != None:
-           print "  " * level, "<" + child.name+ ">"
+           print("  " * level, "<" + child.name+ ">")
            descend(child, level+1)
-           print "  " * level, "</"+ child.name+ ">"
+           print("  " * level, "</"+ child.name+ ">")
         else:
-           print "  " * level, " " + child.string
+           print("  " * level, " " + child.string)
 
 descend(soup, 0)
