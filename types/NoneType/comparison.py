03fab8bcde3c2be6d4a0a588d4828d7811bc5c0e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-04-18 10:17:12 +0200

    + comparison.py

---
+++ types/NoneType/comparison.py
@@ -0,0 +1,15 @@
+x = 42
+y = None
+
+if x == y:
+   print("they're the same")
+else:
+   print("they differ")
+
+x = None
+y = None
+
+if x == y:
+   print("they're the same")
+else:
+   print("they differ")
