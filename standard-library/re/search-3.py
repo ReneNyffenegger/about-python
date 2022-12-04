adefe4a8e105b6c35c8fe3fc36ca3c28cfd3c91d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-12-04 19:21:51 +0100

    use match := re.search

--- standard-library/re/search-3.py
+++ standard-library/re/search-3.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python
-
 import re
 
-match = re.search('foo:\s+(\d+),\s+bar:\s+(\d+)', 'hello foo:   42, bar:  1001xyz')
-if match:
+if match := re.search('foo:\s+(\d+),\s+bar:\s+(\d+)', 'hello foo:   42, bar:  1001xyz'):
+
    print (match.group() )  # foo:   42, bar:  1001
    print (match.group(1))  # 42
    print (match.group(2))  # 1001
