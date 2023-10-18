0d6777a15e5936054d87912f148c26151036e40e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-10-18 21:30:11 +0200

    Add comment

--- libraries/ScaNN/example/builder.py
+++ libraries/ScaNN/example/builder.py
@@ -1,5 +1,5 @@
 builder = scann.scann_ops_pybind.builder(
     data,
     num_neighbors    =  10,
-    distance_measure = 'squared_l2'
+    distance_measure = 'squared_l2'  # or, alternatively: 'dot_product'
 )
