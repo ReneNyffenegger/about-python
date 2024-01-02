9b2e170fa44839f0d2280e2127a7a0917af4dcf9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-01-02 20:03:19 +0100

    add async def

--- built-in/functions/type.py
+++ built-in/functions/type.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
 
-print ( type( []                    ))         # <class 'list'>
-print ( type( ['foo', 1 ]           ))         # <class 'list'>
-print ( type( {}                    ))         # <class 'dict'>
-print ( type( ''                    ))         # <class 'str'>
-print ( type( 1 == 0                ))         # <class 'bool'>
-print ( type( True                  ))         # <class 'bool'>
-print ( type( str  ('foo')          ))         # <class 'str'>
-print ( type( bytes('foo', 'utf-8') ))         # <class 'bytes'>
-print ( type( 'bar'                 ))         # <class 'str'>
-print ( type(  0                    ))         # <class 'int'>
-print ( type(  42                   ))         # <class 'int'>
-print ( type(  42.42                ))         # <class 'float'>
-print ( type(  None                 ))         # <class 'NoneType'>
-print ( type( lambda a: a           ))         # <class 'function'>
-print ( type( type( 0 )             ))         # <class 'type'>
-print ( type( list                  ))         # <class 'type'>
-print ( type( dict                  ))         # <class 'type'>
-print ( type( type.__dict__         ))         # <class 'mappingproxy'>
+print ( type( []                      ))  # <class 'list'>
+print ( type( ['foo', 1 ]             ))  # <class 'list'>
+print ( type( {}                      ))  # <class 'dict'>
+print ( type( ''                      ))  # <class 'str'>
+print ( type( 1 == 0                  ))  # <class 'bool'>
+print ( type( True                    ))  # <class 'bool'>
+print ( type( str  ('foo')            ))  # <class 'str'>
+print ( type( bytes('foo', 'utf-8')   ))  # <class 'bytes'>
+print ( type( 'bar'                   ))  # <class 'str'>
+print ( type(  0                      ))  # <class 'int'>
+print ( type(  42                     ))  # <class 'int'>
+print ( type(  42.42                  ))  # <class 'float'>
+print ( type(  None                   ))  # <class 'NoneType'>
+print ( type( lambda a: a             ))  # <class 'function'>
+print ( type( type( 0 )               ))  # <class 'type'>
+print ( type( list                    ))  # <class 'type'>
+print ( type( dict                    ))  # <class 'type'>
+print ( type( type.__dict__           ))  # <class 'mappingproxy'>
 
 print ("---")
 
 def   f(): pass
-print ( type( f.__code__            ))         # <class 'code'>
+print ( type( f.__code__              ))  # <class 'code'>
 
+async def a(): pass
+aa = a()
+print ( type(aa                       ))  # <class 'coroutine'>
 
 class class_A(object): pass
-print ( type(class_A          ))         # <type 'type'>
+print ( type(class_A                  ))  # <class 'type'>
 
 instance_A=class_A()
-print ( type(instance_A       ))         # <class '__main__.class_A'>
+print ( type(instance_A               ))  # <class '__main__.class_A'>
 
-print ( type(instance_A) is class_A )    # True
+print ( type(instance_A) is class_A    )  # True
 
 class class_B(class_A): pass
 
